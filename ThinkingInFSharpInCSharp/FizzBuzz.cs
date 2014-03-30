e63dabe9cc4baa9cca6e90566592d829146cc507
Mike Clement <mike@softwareontheside.com>
2014-03-29 23:49:31 -0600

    C# multiple FizzBuzz implementations F# multiple FizzBuzz implementations

--- ThinkingInFSharpInCSharp/FizzBuzz.cs
+++ ThinkingInFSharpInCSharp/FizzBuzz.cs
@@ -16,6 +16,9 @@ namespace ThinkingInFSharpInCSharp
         public void Translate(int input, string expected)
         {
             Assert.That(FizzBuzzTranslator.Translate(input), Is.EqualTo(expected));
+            Assert.That(FizzBuzzTranslator.TranslateExtractedMethods(input), Is.EqualTo(expected));
+            Assert.That(FizzBuzzTranslator.TranslateBuild(input), Is.EqualTo(expected));
+            Assert.That(FizzBuzzTranslator.TranslateBuildPrivateMethods(input), Is.EqualTo(expected));
         }
 
         [Test]
@@ -36,6 +39,64 @@ namespace ThinkingInFSharpInCSharp
             if (input % 3 == 0) return "Fizz";
             if (input % 5 == 0) return "Buzz";
             return input.ToString();
+        }
+
+        public static string TranslateExtractedMethods(int input)
+        {
+            if (ShouldFizz(input) && ShouldBuzz(input)) return "FizzBuzz";
+            if (ShouldFizz(input)) return "Fizz";
+            if (ShouldBuzz(input)) return "Buzz";
+            return input.ToString();
+        }
+
+        private static bool ShouldFizz(int input)
+        {
+            return input % 3 == 0;
+        }
+
+        private static bool ShouldBuzz(int input)
+        {
+            return input % 5 == 0;
+        }
+
+        public static string TranslatePrivateMethods(int input)
+        {
+            Func<int, bool> shouldFizz = i => i % 3 == 0;
+            Func<int, bool> shouldBuzz = i => i % 5 == 0;
+            if (shouldFizz(input) && shouldBuzz(input)) return "FizzBuzz";
+            if (shouldFizz(input)) return "Fizz";
+            if (shouldBuzz(input)) return "Buzz";
+            return input.ToString();
+        }
+
+        public static string TranslateBuild(int input)
+        {
+            string output = "";
+            output = Fizzy(input, output);
+            output = Buzzy(input, output);
+            output = Other(input, output);
+            return output;
+        }
+
+        private static string Fizzy(int input, string inputString)
+        {
+            return inputString + (input % 3 == 0 ? "Fizz" : "");
+        }
+        private static string Buzzy(int input, string inputString)
+        {
+            return inputString + (input % 5 == 0 ? "Buzz" : "");
+        }
+        private static string Other(int input, string inputString)
+        {
+            return inputString == "" ? input.ToString() : inputString;
+        }
+
+        public static string TranslateBuildPrivateMethods(int input)
+        {
+            Func<int, string, string> fizzy = (i, s) => s + (i % 3 == 0 ? "Fizz" : "");
+            Func<int, string, string> buzzy = (i, s) => s + (i % 5 == 0 ? "Buzz" : "");
+            Func<int, string, string> other = (i, s) => s == "" ? i.ToString() : s;
+            return other(input, buzzy(input, fizzy(input, "")));
         }
     }
 }
