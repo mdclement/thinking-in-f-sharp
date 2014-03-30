b93d2a1db104833a75ed4e7c6379421597984d86
Mike Clement <mike@softwareontheside.com>
2014-03-29 22:19:17 -0600

    initial setup C# simple implementation F# tests, no implementation

---
+++ ThinkingInFSharp/FizzBuzz.fs
@@ -0,0 +1,28 @@
+﻿namespace ThinkingInFSharp
+
+open NUnit.Framework
+open FsUnit
+
+type FizzBuzzTranslator() =
+    static member Translate(input) =
+        ()
+
+
+[<TestFixture>] 
+type ``FizzBuzz Tests`` ()=
+    [<TestCase(1, "1")>]
+    [<TestCase(2, "2")>]
+    [<TestCase(3, "Fizz")>]
+    [<TestCase(5, "Buzz")>]
+    [<TestCase(6, "Fizz")>]
+    [<TestCase(10, "Buzz")>]
+    [<TestCase(15, "FizzBuzz")>]
+    member x.
+        ``input is expected`` (input : int, expected : string)= 
+                FizzBuzzTranslator.Translate(input) |> should equal expected
+
+    [<Test>]
+    member x.
+        ``execute`` ()=
+            let result = {1 .. 100} |> Seq.map(fun n -> FizzBuzzTranslator.Translate(n))
+            printfn "%A " (Seq.toList result)
---
+++ ThinkingInFSharpInCSharp/FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ThinkingInFSharpInCSharp
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        public void Translate(int input, string expected)
+        {
+            Assert.That(FizzBuzzTranslator.Translate(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void OneToOneHundred()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzTranslator.Translate(i));
+            }
+        }
+    }
+
+    public class FizzBuzzTranslator
+    {
+        public static string Translate(int input)
+        {
+            if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+            if (input % 3 == 0) return "Fizz";
+            if (input % 5 == 0) return "Buzz";
+            return input.ToString();
+        }
+    }
+}
